06e7503463f1e5f7ee6668cde55ad0037ba801fc
Matthew Muccio <me@matthewmuccio.com>
2017-08-11 00:06:09 -0400

    Set up local server and user database and tested connection to it in PHP.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	// Connection to MySQL database quickfeed at address localhost with default username root and no password.
+	$conn = mysqli_connect("localhost", "root", "", "quickfeed");
+
+	// Checks and detects error connecting to database, and displays message if so.
+	if (mysqli_connect_errno())
+		echo "Failed to connect: " . mysqli_connect_errno();
+
+	// Using connection variable, insert test user into table.
+	$query = mysqli_query($conn, "INSERT INTO test VALUES ('1', 'Matthew')");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quickfeed - Welcome</title>
+</head>
+<body>
+	<p>Quickfeed</p>
+</body>
+</html>
